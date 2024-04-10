6585cd599c88e000c79f7ea19f0741112c049d5f
conuaz <lucas.olasz@tty2000.com.br>
2024-04-10 18:33:19 -0300

    Primeiro commit

---
+++ src/main/java/me/dio/javarestfull/JavaRestfullApplication.java
@@ -0,0 +1,12 @@
+package me.dio.javarestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestfullApplication.class, args);
+	}
+}
---
+++ src/test/java/me/dio/javarestfull/JavaRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.javarestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
