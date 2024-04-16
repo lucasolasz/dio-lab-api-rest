e0183eb616cc13d9f9a9836ed745401092bca18f
conuaz <lucas.olasz@tty2000.com.br>
2024-04-16 14:18:28 -0300

    Configuração do CORS

--- src/main/java/me/dio/javarestfull/JavaRestfullApplication.java
+++ src/main/java/me/dio/javarestfull/JavaRestfullApplication.java
@@ -1,8 +1,12 @@
 package me.dio.javarestfull;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class JavaRestfullApplication {
 
